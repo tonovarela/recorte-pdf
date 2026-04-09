f099314cb269ef2b64bcffe153362bd8766ea3b1
tonovarela <tonovarela@live.com>
2026-04-09 17:35:46 -0600

    Mejora la legibilidad del código al reordenar el espaciado en Main y agregar un mensaje de carga en Conf

--- conf/Conf.cs
+++ conf/Conf.cs
@@ -8,8 +8,8 @@ public class Conf
     
     public Conf()
     {
-        Env.Load();
         Console.WriteLine("Cargando configuración...");
+        Env.Load();        
         Environment = Env.GetString("ENVIRONMENT") ?? throw new InvalidOperationException("La variable de entorno 'ENVIRONMENT' no está definida.");
         bool isDevelopment = Environment.Equals("development", StringComparison.OrdinalIgnoreCase);        
         Console.WriteLine($"Entorno: {(isDevelopment ? "Desarrollo" : "Producción")}");
